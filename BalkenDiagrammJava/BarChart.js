7328862a25fba33427f814e2e9c5afb72db04bb0
Sven Ostkotte <Sven.Ostkotte@drheuer.de>
2024-11-28 10:48:25 +0100

    Balken

---
+++ BalkenDiagrammJava/BarChart.js
@@ -0,0 +1,35 @@
+﻿
+
+export class BarChart {
+    _dataObject;
+    _canvas;
+    constructor(dataObject, canvas) {
+        this._dataObject = dataObject;
+        this._canvas = canvas;
+    }
+
+    DrawChart() {
+
+        let color = ["blue", "red", "green", "grey"];
+        let y = 0;
+        let i = 0;
+        for (let data in this._dataObject) {
+
+            this._canvas.fillStyle = color[i];
+            this._canvas.fillRect(0, y, this._dataObject[data] * 10, 20);
+            y += 20;
+            i++;
+        }
+        y = 0;
+        i = 0;
+        for (let data in this._dataObject) {
+
+
+            this._canvas.fillStyle = "white";
+            this._canvas.fillText(data, 0, y + 15, 250);
+            y += 20;
+            i++;
+        }
+
+    }
+}
--- BalkenDiagrammJava/script.js
+++ BalkenDiagrammJava/script.js
@@ -1,1 +1,24 @@
-﻿
+﻿import { BarChart } from "./BarChart.js";
+
+window.onload = function () {
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+
+    
+
+
+    let daten =
+    {
+        "C#": 20,
+        "Java": 50,
+        "C++": 30,
+        "JavaScript": 20
+    };
+
+
+
+    let a = new BarChart(daten, context);
+    a.DrawChart();
+}
+
+
